57062b27ca4d35cd963dc2ddc5d067b90acb6737
Billy Nisbett <william.nisbett@gmail.com>
2023-03-17 10:17:45 +0000

    fix leap for years divisible by 400

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -2,7 +2,7 @@
 
 namespace leap {
 bool is_leap_year(int n) {
-    if (n % 400 == 0) { return false; }
+    if (n % 400 == 0) { return true; }
     if (n % 100 == 0) { return false; }
     if (n % 4 == 0) { return true; }
     return false;
