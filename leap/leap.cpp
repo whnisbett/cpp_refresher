fdea353a7b719b7b9c2ff98fbe5d5bdb3f976e89
Billy Nisbett <william.nisbett@gmail.com>
2023-03-17 10:00:18 +0000

    add leap exercise

---
+++ leap/leap.cpp
@@ -0,0 +1,10 @@
+#include "leap.h"
+
+namespace leap {
+bool is_leap_year(int n) {
+    if (n % 400 == 0) { return false; }
+    if (n % 100 == 0) { return false; }
+    if (n % 4 == 0) { return true; }
+    return false;
+}
+}  // namespace leap
