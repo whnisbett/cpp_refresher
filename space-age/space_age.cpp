7644c895857efe8b634067568683ff719b63b84b
Billy Nisbett <william.nisbett@gmail.com>
2023-03-19 22:30:12 +0100

    implement definitions for and complete space_age

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -11,4 +11,53 @@ const double saturn_factor = 29.447498;
 const double uranus_factor = 84.016846;
 const double neptune_factor = 164.79132;
 
+space_age::space_age(long secs) : secs_(secs)
+{
+}
+
+long space_age::seconds() const
+{
+    return secs_;
+}
+
+double space_age::on_earth() const
+{
+    return seconds() * earth_years_per_second;
+}
+
+double space_age::on_jupiter() const
+{
+    return on_earth() / jupiter_factor;
+}
+
+double space_age::on_mars() const
+{
+    return on_earth() / mars_factor;
+}
+
+double space_age::on_venus() const
+{
+    return on_earth() / venus_factor;
+}
+
+double space_age::on_mercury() const
+{
+    return on_earth() / mercury_factor;
+}
+
+double space_age::on_neptune() const
+{
+    return on_earth() / neptune_factor;
+}
+
+double space_age::on_saturn() const
+{
+    return on_earth() / saturn_factor;
+}
+
+double space_age::on_uranus() const
+{
+    return on_earth() / uranus_factor;
+}
+
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -7,16 +7,16 @@ class space_age {
     private:
         double secs_;
     public:
-        space_age(double secs);
-        double seconds();
-        double on_earth();
-        double on_mercury();
-        double on_venus();
-        double on_mars();
-        double on_jupiter();
-        double on_saturn();
-        double on_uranus();
-        double on_neptune();
+        space_age(long secs);
+        long seconds() const;
+        double on_earth() const;
+        double on_mercury() const;
+        double on_venus() const;
+        double on_mars() const;
+        double on_jupiter() const;
+        double on_saturn() const;
+        double on_uranus() const;
+        double on_neptune() const;
 };
 
 }  // namespace space_age
