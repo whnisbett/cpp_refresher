187ea6c15bd00dc5edab542e173c1d820fc67e39
Billy Nisbett <william.nisbett@gmail.com>
2023-03-24 23:42:31 +0100

    implement add method for grade school

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -1,5 +1,16 @@
 #include "grade_school.h"
+#include <algorithm>
 
 namespace grade_school {
+
+    void school::add(std::string name, int grade) {
+        std::vector<std::string> students = roster_[grade];
+        students.insert(
+            // finds first student in grade who name is after current name
+            std::upper_bound(begin(students), end(students), name),
+            name
+        );
+
+    }
 
 }  // namespace grade_school
