22759ec070b61a92c69e96c3baabc0e1c286eee2
Billy Nisbett <william.nisbett@gmail.com>
2023-04-07 18:34:22 +0100

    reformat

--- prime-factors/prime_factors.cpp
+++ prime-factors/prime_factors.cpp
@@ -1,23 +1,25 @@
 #include "prime_factors.h"
 
-namespace prime_factors {
+namespace prime_factors
+{
 
     std::vector<int> of(unsigned int n)
     {
-
     }
-    
+
     bool is_prime(unsigned int n)
     {
+        if (n % 2 == 0)
+            return false;
+
         int range{std::floor(std::sqrt(n))};
         int i{3};
-        
         while (i <= range)
         {
             if (n % i == 0)
                 return false;
-            i+=2;
+            i += 2;
         }
         return true;
     }
-}  // namespace prime_factors
+} // namespace prime_factors
